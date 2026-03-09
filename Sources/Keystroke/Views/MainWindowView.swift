6134d74e68a4bc5ce59192ae0495d7d8c5f7f330
leochiu <leochiu.life@gmail.com>
2026-03-09 20:06:55 +0800

    feat: add MainWindowView with sidebar and content layout

---
+++ Sources/Keystroke/Views/MainWindowView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct MainWindowView: View {
+    @ObservedObject var tracker: MouseTracker
+    @State private var selectedItem: SidebarItem = .defaultItem
+
+    var body: some View {
+        HStack(spacing: 0) {
+            SidebarView(selectedItem: $selectedItem, tracker: tracker)
+
+            Divider()
+
+            contentView
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+    }
+
+    @ViewBuilder
+    private var contentView: some View {
+        switch selectedItem {
+        case .cursor:
+            CursorSettingsView(tracker: tracker)
+        }
+    }
+}
