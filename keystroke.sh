79f0189513900da4d6da79eade82316b336f78cc
leochiu <leochiu.life@gmail.com>
2026-03-21 22:22:42 +0800

    feat: add initial keystroke script with Raycast parameters

---
+++ keystroke.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title keystroke
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description run dev.sh
+
+./Scripts/dev.sh
